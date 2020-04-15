bac5d4a6349c7caf7555918f2aa841abe21949c3
duarte-santos <duarte.coelho.santos@gmail.com>
2020-04-15 18:40:07 +0100

    F1.1 Set up routing and page, closes #93

---
+++ frontend/src/views/student/question/StudentQuestionView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>My Questions View</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class StudentQuestionView extends  Vue {}
+</script>
+
+<style lang="scss" scoped></style>
